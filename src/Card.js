e762423619676f2bc49eb2d2cedcc6552cf76476
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-12-12 18:39:24 -0500

    Card component

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = (props) => {
+  // destructuring
+  const { name, email, id } = props;
+  return (
+    <div className="tc bg-light-clear dib br4 pa1 ma3 grow pw2 shadow-5 ba b--near-white">
+      <img alt="robot" src={`https://robohash.org/${id}?100x100`} />
+      <div>
+        <h1>{name}</h1>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
