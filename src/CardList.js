e150400565fa3bdf24ca6d9383758e1289b656d7
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-12-13 16:44:18 -0500

    modify CardList Component

--- src/CardList.js
+++ src/CardList.js
@@ -2,11 +2,16 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  return (
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    </div>
-  );
+  const cardComponent = robots.map((user, _index) => {
+    return (
+      <Card
+        key={_index}
+        id={robots[_index].id}
+        name={robots[_index].name}
+        email={robots[_index].email}
+      />
+    );
+  });
+  return <div>{cardComponent}</div>;
 };
 export default CardList;
