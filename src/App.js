2c4dfff90bc955406d2453f89857590484ed7ea3
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-12-18 19:15:46 -0500

    add scroll component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
   // declaring the state inside constructor
@@ -38,7 +39,10 @@ class App extends Component {
         <div className="tc">
           <h1 className="f1">Robot Search</h1>
           <SearchBox searchChange={this.robotsSearchChange}></SearchBox>
-          <CardList robots={filterRobots} />
+          {/* Scroll - Wrap Component */}
+          <Scroll>
+            <CardList robots={filterRobots} />
+          </Scroll>
         </div>
       );
     }
