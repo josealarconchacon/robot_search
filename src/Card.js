efb25537fc44389591caf610a8005247b2bbf650
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-12-18 19:03:37 -0500

    add new component

--- src/Card.js
+++ src/Card.js
@@ -4,8 +4,8 @@ const Card = (props) => {
   // destructuring
   const { name, email, id } = props;
   return (
-    <div className="tc bg-light-clear dib br4 pa5 ma2 grow pw2 shadow-5 ba b--near-white">
-      <img alt="robot" src={`https://robohash.org/${id}?100x100`} />
+    <div className="tc bg-light-clear dib br4 pa4 ma3 grow pw2 shadow-5 ba b--near-white">
+      <img alt="robot" src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h1>{name}</h1>
         <p>{email}</p>
